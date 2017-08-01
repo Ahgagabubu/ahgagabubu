44b6b105cb6c65d5d3e856aedecd2ee60d122573
Jasper Yang <jasperyang@bovia.com.tw>
2017-08-01 11:12:54 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+echo "!!! This step will overwrite your vimrc & tmux.conf                              !!!"
+echo "!!! Make sure you have backed up your configs if you want to restore them later. !!!"
+read -p "Do you want to continue? [y/N]" yn
+
+case $yn in
+    [Yy]*)
+        mkdir -p ~/.vim/colors
+        cp -rf vimrc ~/.vimrc
+        cp -rf vim_colors/* ~/.vim/colors
+
+        mkdir -p ~/.tmux/tmux-themepack
+        cp -rf tmux.conf ~/.tmux.conf
+        cp -rf tmux_colors/* ~/.tmux/tmux-themepack
+        echo "Done!"
+    ;;
+
+    *) 
+        echo "Do nothing & exit!"
+        exit
+    ;;
+esac
