4986d0208cc1a00e82d5c1aa5d6f7732962191d3
Jasper <jasper@test.bovia.com.tw>
2017-08-01 15:46:42 +0800

    rename and update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,62 @@
-#!/bin/sh
-echo "!!! This step will overwrite your vimrc & tmux.conf                              !!!"
-echo "!!! Make sure you have backed up your configs if you want to restore them later. !!!"
-read -p "Do you want to continue? [y/N]" yn
+#########################################################
+# This script can only run on bash or zsh               #
+#########################################################
 
-case $yn in
-    [Yy]*)
-        mkdir -p ~/.vim/colors
-        cp -rf vimrc ~/.vimrc
-        cp -rf vim-dracula/colors/* ~/.vim/colors/
+function printline(){
+    echo    "------------------------------------------------------------------------------------"
+}
 
-        mkdir -p ~/.tmux/tmux-themepack
-        cp -rf tmux.conf ~/.tmux.conf
-        cp -rf tmux-themepack ~/.tmux/
-        echo "Done!"
-    ;;
+echo    ""
+printline
+echo    "!!! This step will overwrite your vimrc & tmux.conf                              !!!"
+echo    "!!! Make sure you have backed up your configs if you want to restore them later. !!!"
+printline
 
-    *) 
-        echo "Do nothing & exit!"
-        exit
-    ;;
-esac
+read -p " Do you want to continue? [y/N] " CONTINUE
+
+if [[ $CONTINUE != [Yy] ]];then
+    echo " Do nothing & exit!"
+    exit
+fi
+
+read -p " Install all components? (Choose no you can select what you want later.) [Y/n] " INSTALL_ALL
+[[ $INSTALL_ALL != [Nn] ]] && INSTALL_ALL=y
+
+# Check config
+if [[ $INSTALL_ALL != [Yy] ]];then
+    read -p " Install zsh conf? [Y/n] " ZSHRC
+    read -p " Install vim conf? [Y/n] " VIMRC
+    read -p " Install tmux conf? [Y/n] " TMUXRC
+fi
+
+[[ $ZSHRC != [Nn] ]] && ZSHRC=y
+[[ $VIMRC != [Nn] ]] && VIMRC=y
+[[ $TMUXRC != [Nn] ]] && TMUXRC=y
+
+printline
+
+# Git submodule update
+git submodule init
+git submodule update
+
+if [[ $ZSHRC == [Yy] ]];then
+    echo -n " Installing zsh configs..."
+    echo "done!"
+fi
+
+if [[ $VIMRC == [Yy] ]];then
+    echo -n " Installing vim configs..."
+    mkdir -p ~/.vim/colors
+    cp -rf vim-dracula/colors/* ~/.vim/colors/
+    cp -rf vimrc.template ~/.vimrc
+    echo "done!"
+fi
+
+if [[ $TMUXRC == [Yy] ]];then
+    echo -n " Installing tmux configs..."
+    mkdir -p ~/.tmux
+    cp -rf tmux-themepack ~/.tmux/
+    cp -rf tmux.conf.template ~/.tmux.conf
+    echo "done!"
+fi
+
